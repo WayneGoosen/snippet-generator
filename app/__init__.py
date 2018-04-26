c59bf34dd55deae9fa1def7da96fb6f14f3a813c
Wayne Goosen <goosenwayne@gmail.com>
2018-04-26 19:09:58 +0200

    Upgrade to python 3.6.4.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
+from .pages.views import pages_app
 
 def create_app(env='dev'):
     app = Flask(__name__)
     app.config.from_object('app.config.config-%s' % env)
 
     # import blueprints
-    from pages.views import pages_app
-
     # register blueprints
     app.register_blueprint(pages_app)
 
     if app.debug:
-        print 'running in debug mode'
+        print('running in debug mode')
     else:
-        print 'NOT running in debug mode'
+        print('NOT running in debug mode')
     return app
