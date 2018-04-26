77f06f7ab98755b6c6f528d7cb204fac23bda3b4
Wayne Goosen <goosenwayne@gmail.com>
2018-04-26 19:18:24 +0200

    Update procfile and startup for Heroku.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+"""Create an application instance."""
 from app import create_app
 import os
 
-port = int(os.environ.get('PORT', 5000))
+#port = int(os.environ.get('PORT', 5000))
 app = create_app(env='prod') #Or pass 'live' to NOT be in debug mode
-app.run(host='0.0.0.0', port=port)
+#app.run(host='0.0.0.0', port=port)
